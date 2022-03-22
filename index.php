cf3c110ebf7ccf78de2282aa38f92da42ae7161f
hsofee <hsofee@gmail.com>
2022-03-22 22:59:17 +0700

    1 file modified

--- index.php
+++ index.php
@@ -21,7 +21,9 @@
 	$messages = [];
 	$messages['replyToken'] = $replyToken;
 	$messages['messages'][0] = getFormatTextMessage("หวัดดีคะ Miss นมแม่ยินดีให้บริการ");
-	
+	$messages['messages'][0] = getFormatTextMessage("หวัดดีคะ Heroku ");
+
+
 	// เชื่อมต่อกับ ฐานข้อมูล อย่างไร ตรงนี้ 
 	// ทดสอบ
 	// ทดสบการเชื่อมต่อ
