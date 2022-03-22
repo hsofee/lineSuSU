4a8d9e18ad5013fd935ae59fe08845fee9b41a9b
hsofee <hsofee@gmail.com>
2022-03-22 23:00:02 +0700

    1 file modified

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
 	
 	$messages = [];
 	$messages['replyToken'] = $replyToken;
-	$messages['messages'][0] = getFormatTextMessage("หวัดดีคะ Miss นมแม่ยินดีให้บริการ");
+	$messages['messages'][0] = getFormatTextMessage("หวัดดีคะ Miss นมแม่ยินดีให้บริการ1");
 	$messages['messages'][0] = getFormatTextMessage("หวัดดีคะ Heroku ");
 
 
